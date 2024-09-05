3300f1c0b79d5776e1c7ac288a20f0df4078e18c
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-09-05 16:00:56 +0300

    update cors

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,7 +2,7 @@ import cors, { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
   optionsSuccessStatus: 200,
-  origin: ['http://localhost:3000', 'https://polar-oasis-90047-4d8ad94d27f3.herokuapp.com/'],
+  origin: ['http://localhost:3000', 'https://polar-oasis-90047-4d8ad94d27f3.herokuapp.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'credentials'],
   credentials: true,
