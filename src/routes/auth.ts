ef713f275d21637ef2e4db13b065b5a89e0bda7e
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-09-29 19:04:14 +0300

    routh auth - applay the login and googleCallback routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,30 +1,9 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { googleCallback, login } from '../controllers/authControllers';
 
 const router = express.Router();
 
-router.post('/login', async (req: Request, res: Response) => {
-  const { access_token } = req.body;
-
-  try {
-    const response = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
-
-    if (!response.ok) {
-      console.error('Failed to retrieve user information:', response.statusText);
-      return res.status(401).json({ message: 'Invalid Google token' });
-    }
-
-    const userInfo = await response.json();
-    const { email, name, picture } = userInfo;
-    return res.status(200).json({ email, name, picture });
-  } catch (error) {
-    console.error('Error retrieving user information:', error);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-});
+router.get('/login', login);
+router.get('/oauth2callback', googleCallback);
 
 export default router;
