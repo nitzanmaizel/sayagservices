2a67dd2789e9e25fd82b10650c5beaf86d50e312
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-09-29 18:52:54 +0300

    oauth2Client - chnage the scopes and creating of oauth2Client

--- src/config/oauth2Client.ts
+++ src/config/oauth2Client.ts
@@ -1,20 +1,21 @@
 import { OAuth2Client } from 'google-auth-library';
 
-const CLIENT_ID = process.env.CLIENT_ID || '';
-const CLIENT_SECRET = process.env.CLIENT_SECRET || '';
-const REDIRECT_URI = process.env.REDIRECT_URI || '';
+const CLIENT_ID = process.env.CLIENT_ID as string;
+const CLIENT_SECRET = process.env.CLIENT_SECRET as string;
+const REDIRECT_URI = process.env.REDIRECT_URI as string;
 
 if (!CLIENT_ID || !CLIENT_SECRET || !REDIRECT_URI) {
   throw new Error('Missing required environment variables for OAuth2 client');
 }
 
-const oAuth2Client: OAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+export const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
-const SCOPES = [
-  'https://www.googleapis.com/auth/drive',
-  'https://www.googleapis.com/auth/documents',
+export const SCOPES = [
+  'openid',
+  'profile',
+  'email',
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/documents',
+  'https://www.googleapis.com/auth/drive',
 ];
-
-export { oAuth2Client, SCOPES };
