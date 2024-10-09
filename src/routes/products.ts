91a7a61465609dc892b6507166a0b21f0f196cfc
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-09 14:47:33 +0300

    make some of the products api public

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -7,12 +7,15 @@ import {
   deleteProduct,
 } from '../controllers/productControllers';
 import { upload } from '../config/cloudinary';
+import { authenticateJWT, refreshTokenMiddleware } from '../middleware';
 
 const router: Router = Router();
 
-router.post('/', upload.single('image'), createProduct);
 router.get('/', getAllProducts);
 router.get('/:productId', getProductById);
+
+router.use(authenticateJWT, refreshTokenMiddleware);
+router.post('/', upload.single('image'), createProduct);
 router.put('/:productId', upload.single('image'), updateProduct);
 router.delete('/:productId', deleteProduct);
 
